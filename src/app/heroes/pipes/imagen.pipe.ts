0bd21fc1adb79421f214f1a08a82592490b6c760
Francisco blanco <francisco.blanco@grupoemi.com>
2022-08-28 09:24:27 -0500

    Secondary co

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -1,12 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { HeroeReponse } from '../interfaces/heroes.interfaces';
 
 @Pipe({
   name: 'imagen'
 })
 export class ImagenPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(heroe: HeroeReponse): string {
+    return heroe.id === '' || heroe.id == null ? '': '';
   }
 
 }
